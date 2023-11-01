a9b49ee28968874b8706d37e8c0e1fad02263eb6
arif123 <nashrullah2344@gmail.com>
2023-11-02 06:12:37 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,27 +1,51 @@
-require('dotenv').config();
+const mysql = require('mysql2');
+const { promisify } = require('util');
 
-module.exports = {
-  development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.MYSQL_HOST,
-    dialect: process.env.MYSQL_DIALECT,
-    timezone: process.env.MYSQL_TIMEZONE,
-  },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-  production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: process.env.MYSQL_HOST,
-    dialect: process.env.MYSQL_DIALECT,
-    timezone: process.env.MYSQL_TIMEZONE,
-  },
+// Database connection configuration
+const dbConfig = {
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10, // Adjust this limit as needed
+  queueLimit: 0,
 };
+
+// Create a connection pool
+const pool = mysql.createPool(dbConfig);
+
+// Promisify the pool.query method to use async/await
+const query = promisify(pool.query).bind(pool);
+
+// Function to execute a database query with retry logic
+async function executeQueryWithRetry(sql, params, maxRetries = 3) {
+  for (let retry = 0; retry < maxRetries; retry++) {
+    try {
+      const result = await query(sql, params);
+      return result;
+    } catch (error) {
+      if (error.code === 'ETIMEDOUT' && retry < maxRetries - 1) {
+        // Retry the connection on ETIMEDOUT error
+        console.log(`Retry attempt ${retry + 1} after ETIMEDOUT error`);
+        continue;
+      } else {
+        throw error; // Propagate other errors
+      }
+    }
+  }
+  throw new Error(`Max retries reached. Unable to execute query.`);
+}
+
+// Example usage
+async function main() {
+  try {
+    const result = await executeQueryWithRetry('SELECT * FROM your_table');
+    console.log(result);
+  } catch (error) {
+    console.error(`Error executing query: ${error.message}`);
+  }
+}
+
+main();
