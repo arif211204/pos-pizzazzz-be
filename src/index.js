c9c06088e12664d9a7b9f38011c4348522347021
arif123 <nashrullah2344@gmail.com>
2023-11-02 04:15:40 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,21 +30,11 @@ app.use('/vouchers', voucherRouter);
 app.use('/variants', variantController);
 app.use('/transVariant', transactionVariantController);
 
-// const server = http.createServer(app);
-// app.listen(PORT, () => {
-//   console.log(`listen on port:${PORT}`);
-//   // db.sequelize.sync({ alter: true });
-// });
-const server = http.createServer((req, res) => {
-  // Handle any other HTTP requests if needed
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('hello world');
+const server = http.createServer(app);
+app.listen(PORT, () => {
+  console.log(`listen on port:${PORT}`);
+  // db.sequelize.sync({ alter: true });
 });
-
-// Mount the Express app on the existing server
-server.on('request', app);
-
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Server running on port ${PORT}`);
 });
