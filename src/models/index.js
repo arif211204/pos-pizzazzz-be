66c799d91fc65e921a36dd7177f5c22de50919bf
arif123 <nashrullah2344@gmail.com>
2023-11-02 03:19:03 +0700

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -7,7 +7,7 @@ const process = require('process');
 const { createPool } = require('mysql2/promise');
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 const config = require(`${__dirname}/../config/config.js`)[env];
 
 const db = {};
@@ -23,7 +23,7 @@ if (config.use_env_variable) {
 
 const mysqlConnection = createPool({
   host: process.env.MYSQL_HOST,
-  port: process.env.PORT,
+  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
