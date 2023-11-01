62d25a40fdae33f9fb223da714365fce9faa18a1
arif123 <nashrullah2344@gmail.com>
2023-11-01 16:29:39 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -17,10 +17,11 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    dialect: process.env.MYSQL_DIALECT,
+    timezone: process.env.MYSQL_TIMEZONE,
   },
 };
