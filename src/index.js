fa2c498376531366c6827632bd7c1b9feeed2774
arif123 <nashrullah2344@gmail.com>
2023-11-02 06:03:10 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,11 +20,11 @@ const db = require('./models');
 const PORT = process.env.PORT || 2500;
 
 const options = {
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'my_database',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   timeout: 10000, // 10 seconds
 };
 const connection = mysql.createConnection(options);
