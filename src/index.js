3f928bce1c1c07e50bdc3251f0da4f015180a63e
arif123 <nashrullah2344@gmail.com>
2023-11-02 15:14:50 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,11 +19,11 @@ const db = require('./models');
 const PORT = process.env.PORT || 2500;
 
 const options = {
-  host: 'viaduct.proxy.rlwy.net',
-  port: 53700,
-  user: 'root',
-  password: '-daeACE2H--hCg63H22Bg6H3AG-G44gF',
-  database: 'railway',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 };
 
 const connection = mysql.createConnection(options);
