e9a9a9d9fdbc1a699a01b11c66439edb4aa40534
arif123 <nashrullah2344@gmail.com>
2024-02-20 23:15:12 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable consistent-return */
 require('dotenv').config();
 const cors = require('cors');
@@ -14,18 +15,19 @@ const {
   variantController,
   transactionVariantController,
 } = require('./routes');
-const {MYSQL_HOST,MYSQL_DATABASE,MYSQL_PASSWORD,MYSQL_PORT,MYSQL_USER,PORT} = process.env
+
+const {
+  MYSQL_HOST,
+  MYSQL_DATABASE,
+  MYSQL_PASSWORD,
+  MYSQL_PORT,
+  MYSQL_USER,
+  PORT,
+} = process.env;
+
 const db = require('./models');
 
 const port = PORT || 2700;
-
-const pool = mysql.createPool({
-  host: MYSQL_HOST,
-  port: MYSQL_PORT,
-  user: MYSQL_USER,
-  password: MYSQL_PASSWORD,
-  database: MYSQL_DATABASE,
-});
 
 const app = express();
 app.use(cors());
@@ -39,34 +41,12 @@ app.use('/vouchers', voucherRouter);
 app.use('/variants', variantController);
 app.use('/transVariant', transactionVariantController);
 
-
 app.get('/home', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
-})
+});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
   // db.sequelize.sync({ alter: true });
 });
 console.log(process.version, 'version');
-
 // Use a connection pool and handle errors properly
-app.use((req, res, next) => {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error getting database connection:', err);
-      return res.status(500).json({ error: 'Database connection error' });
-    }
-
-    // Attach the connection to the request for use in route handlers
-    req.dbConnection = connection;
-    next();
-  });
-});
-
-// Handle releasing the database connection after handling the request
-app.use((req, res, next) => {
-  if (req.dbConnection) {
-    req.dbConnection.release();
-  }
-  next();
-});
