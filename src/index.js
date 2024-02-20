61a64b675e0b80cae36d5b6bde8cf47b735724ce
arif123 <nashrullah2344@gmail.com>
2024-02-20 23:03:32 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,11 +20,11 @@ const db = require('./models');
 const PORT = process.env.PORT || 2500;
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: 'roundhouse.proxy.rlwy.net',
+  port: 34177,
+  user: 'root',
+  password: 'FCabFFdfaE5edAhec3cAeBc664gHFaed',
+  database: 'railway',
 });
 
 const app = express();
